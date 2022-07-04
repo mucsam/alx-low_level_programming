aab738666d679d395991930c3c7f1b44e8c261bb
Samuel <muchokisamuel@gmail.com>
2022-07-04 15:05:34 +0000

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,5 +6,18 @@
  */
 int main(void)
 {
+	int num;
+	char c;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar((num % 10) + '0');
+	}
+
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
 	return (0);
 }
