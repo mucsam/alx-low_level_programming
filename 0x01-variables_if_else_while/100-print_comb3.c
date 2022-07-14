be8b0152bcefb18dc8d8f192fb26ef3e36ba906f
Samuel <muchokisamuel@gmail.com>
2022-07-14 20:24:00 +0000

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,7 +6,19 @@
  */
 int main(void)
 {
-	
+	int i;
+	int j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			putchar(j);
+		}
+		putchar(i);
+		putchar(',');
+		putchar(' ');
+	}
 	putchar('\n');
 	return (0);
 }
